0cf32a6d7392b48677e865b1ecf0687fa6900db9
Irene Vega <i.vlago@alumnos.upm.es>
2020-11-24 11:12:36 +0100

    base complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PreferencesComponent } from './preferences/preferences.component';
+import { PlayComponent } from './play/play.component';
+import { RecordsComponent } from './records/records.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    PreferencesComponent,
+    PlayComponent,
+    RecordsComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
